0e2f07562a378ad158ba2ad595f3cc90f826ac3d
liran <liran9800@gmail.com>
2025-12-27 12:43:24 +0200

    python sec 22 if , else

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello World")
+print("welcome to the rollercoaster!")
+
+height = int(input("whats your height in cm ?"))
+
+if(height >= 120):
+    print("you can ride the rollerCoaster")
+else: print("you have to wait to grow before u can ride ")
